7c2ec52c61399ac22d6eea6d412bd7141c197841
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2022-06-19 14:06:15 +0200

    fix(orai class):add explorer links

--- src/networkClasses/cosmosNetworks/OraiClass.js
+++ src/networkClasses/cosmosNetworks/OraiClass.js
@@ -4,4 +4,14 @@ export default class OraiNetwork extends BaseCosmosNetwork {
   constructor(walletInfo) {
     super(walletInfo)
   }
+
+  // own explorer link
+  getScannerLinkById() {
+    return `https://scan.orai.io/account${this.address}`
+  }
+
+  // own explorer link
+  getTransactionURLByHash(hash) {
+    return `https://scan.orai.io/txs/${hash}`
+  }
 }

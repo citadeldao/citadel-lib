a73dae1e4e6a7162dc3d0546779531d561ef5be0
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2022-06-21 11:35:47 +0200

    fix(orai class): fix signature

--- src/networkClasses/cosmosNetworks/OraiClass.js
+++ src/networkClasses/cosmosNetworks/OraiClass.js
@@ -5,6 +5,19 @@ export default class OraiNetwork extends BaseCosmosNetwork {
     super(walletInfo)
   }
 
+  async signTransaction(rawTransaction, { privateKey, derivationPath }) {
+    // TODO: Удалить rawTransaction.transaction.transaction после фикса бэка
+    const transaction =
+      rawTransaction.transaction.transaction ||
+      rawTransaction.transaction ||
+      rawTransaction
+    if (this.type === WALLET_TYPES.LEDGER) {
+      return await signTxByLedger(transaction, derivationPath, this.publicKey)
+    }
+    // old signer without publicKey
+    return signTxByPrivateKey(transaction, privateKey)
+  }
+
   // own explorer link
   getScannerLinkById() {
     return `https://scan.orai.io/account/${this.address}`

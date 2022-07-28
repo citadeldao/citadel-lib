dda8c99ecffcf4e126a87d2bc0a9e519be855e0f
Sergey Romanov <serega_rom@bk.ru>
2022-07-28 23:16:14 +0400

    fix: disable balance loading for extension

--- src/generalFunctions/initializeLibrary/backgroundUpdates.js
+++ src/generalFunctions/initializeLibrary/backgroundUpdates.js
@@ -15,31 +15,32 @@ export const backgroundUpdates = async (initialCacheManager) => {
   // configure modules by updated cache and walletList from 'detail' request to fast init
   configureModulesByCaches(initialCacheManager.getInitialCaches())
 
-  // get detailed wallet balances
-  const { data: detailedAccountwallets } = await api.requests.getWalletsDetail()
-
-  // update walletList detail
-  await walletsManager.updateWalletList(detailedAccountwallets)
-
-  // get updated walletList
-  const walletList = walletsManager.getWalletList()
-
-  // update subtokensLists
-  await Promise.all(
-    walletList.map(async (walletInfo) => {
-      // update all subtokensList
-      await walletInstances
-        .getWalletInstanceById(walletInfo.id)
-        .updateSubtokensList()
-    })
-  )
-
   // if isExtension
   if (state.getState('isExtension')) {
     // get local wallets from extension store
     const localWallets = await getExstensionLocalWallets()
     // set local wallets with types to lib
     await walletsManager.setWalletList(localWallets)
+  } else {
+    // get detailed wallet balances
+    const { data: detailedAccountwallets } =
+      await api.requests.getWalletsDetail()
+
+    // update walletList detail
+    await walletsManager.updateWalletList(detailedAccountwallets)
+
+    // get updated walletList
+    const walletList = walletsManager.getWalletList()
+
+    // update subtokensLists
+    await Promise.all(
+      walletList.map(async (walletInfo) => {
+        // update all subtokensList
+        await walletInstances
+          .getWalletInstanceById(walletInfo.id)
+          .updateSubtokensList()
+      })
+    )
   }
 
   // EVENT: inform the client that it is time to update wallet list

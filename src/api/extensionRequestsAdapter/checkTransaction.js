2148facb606d73c5d7dfafbbdf5e758b577143b1
Sergey Romanov <serega_rom@bk.ru>
2022-08-25 09:59:56 +0400

    fix: checkTransaction adapter

--- src/api/extensionRequestsAdapter/checkTransaction.js
+++ src/api/extensionRequestsAdapter/checkTransaction.js
@@ -1,4 +1,6 @@
-// return a stub
-export const checkTransaction = () => {
-  return { data: { status: 'ok' } }
-}
+// function returns request parameters for the axios instance.
+export const checkTransaction = ({ net, hash }) => ({
+  // backend domain is in the axios instance
+  url: `/blockchain/${net}/check/${hash}`,
+  method: 'get',
+})

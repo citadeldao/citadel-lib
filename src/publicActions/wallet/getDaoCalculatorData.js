a9815763861ddc1fe7e4eed8a07141af861d1be6
Sergey Romanov <serega_rom@bk.ru>
2022-08-26 08:30:09 +0400

    add docks

--- src/publicActions/wallet/getDaoCalculatorData.js
+++ src/publicActions/wallet/getDaoCalculatorData.js
@@ -5,6 +5,43 @@ import {
 } from '../../helpers/checkArguments'
 import walletInstances from '../../walletInstances'
 
+/**
+ * Get data to calculate expected rewards (for BSC wallet only)
+ * 
+ * @param walletId STRING, NUMBER (REQUIRED) - BSC wallet id
+ * 
+ * @returns Returns ARRAY
+ * When called outside, result wraps into an object of the form { result: 'success', data: returnedValue, error: null }
+ * @example
+  const response = await citadel.getDaoCalculatorData('224419')
+
+  // =>
+  {
+    result: 'success',
+    data: {
+      bsc_xct: {
+        price: 0.078027802,
+        countAddresses: 1,
+        activeTokens: '3.961124',
+        totalTokens: '33.905134',
+        rewardsPerToken: '0.44200069031042436955',
+        rewardsMonthly: '0.0368333908592020308',
+        checkMonthly: '0.1459016285337657850506192',
+      },
+      cosmos: {
+        countAddresses: 3,
+        activeTokens: '1.508362',
+        totalTokens: '1.538362',
+        price: 24.30150927,
+        rewardsPerToken: '10.7933216941371379688700866505',
+        rewardsMonthly: '0.89944347451142816407',
+        checkMonthly: '1.35668635810100680841295334',
+      },
+    },
+    error: null,
+  }
+ */
+
 export const getDaoCalculatorData = async (walletId) => {
   // checks
   checkInitialization()
--- src/publicActions/wallet/getDaoRewardsByRange.js
+++ src/publicActions/wallet/getDaoRewardsByRange.js
@@ -5,6 +5,41 @@ import {
 } from '../../helpers/checkArguments'
 import walletInstances from '../../walletInstances'
 
+/**
+ * Get rewards for the specified period for each wallet that is linked to the DAO through the given bsc address
+ * 
+ * @param walletId STRING, NUMBER (REQUIRED) - BSC wallet id
+ * @param dateFrom STRING, NUMBER (REQUIRED) - start of period in ms
+ * @param dateTo  STRING, NUMBER (OPTIONAL) - end of period in ms (today by default)
+ * 
+ * @returns Returns ARRAY
+ * When called outside, result wraps into an object of the form { result: 'success', data: returnedValue, error: null }
+ * @example
+  const response = await citadel.getDaoRewardsByRange(
+    '224419',
+    '1641360964145',
+    '1649396164145'
+  )
+
+  // =>
+  {
+    result: 'success',
+    data: [
+      {
+        net: 'juno',
+        address: 'juno1eksvfd7400dr6fvkdh4mshfkukm9u8aqmzcf2v',
+        rewards: 0.00779,
+      },
+      {
+        net: 'icon',
+        address: 'hx759256245a0ae43d2200c9a1e199424b418c710e',
+        rewards: 0.200478,
+      },
+    ],
+    error: null,
+  }
+ */
+
 export const getDaoRewardsByRange = async (walletId, dateFrom, dateTo) => {
   // checks
   checkInitialization()

d9f103f5edf739d57e01db3839f37472f2f2cbce
Sergey Romanov <serega_rom@bk.ru>
2022-08-29 08:43:18 +0400

    feat: setViewingKeyByContract, handle vk fee error

--- src/networkClasses/_BaseNetworkClass/instanceMethods/getTokens.js
+++ src/networkClasses/_BaseNetworkClass/instanceMethods/getTokens.js
@@ -2,5 +2,5 @@ import networkClasses from '../../../networkClasses'
 
 export const getTokens = function () {
   // return array with token names
-  return Object.keys(networkClasses.getNetworkClass(this.net).tokens)
+  return networkClasses.getNetworkClass(this.net).tokens
 }

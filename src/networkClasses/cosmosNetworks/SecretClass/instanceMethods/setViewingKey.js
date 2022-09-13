1fa7e5f08170f2e142c96675879ac361514c30dc
Sergey Romanov <serega_rom@bk.ru>
2022-09-13 17:12:39 +0400

    fix: setViewingKey By Keplr Flow

--- src/networkClasses/cosmosNetworks/SecretClass/instanceMethods/setViewingKey.js
+++ src/networkClasses/cosmosNetworks/SecretClass/instanceMethods/setViewingKey.js
@@ -1,13 +1,10 @@
-import {
-  VIEWING_KEYS_TYPES,
-  // WALLET_TYPES
-} from '../../../../constants'
+import { VIEWING_KEYS_TYPES, WALLET_TYPES } from '../../../../constants'
 import snip20Manager from '../snip20Manager'
 import networkClasses from '../../../'
 import { dispatchLibEvent } from '../../../../generalFunctions/dispatchLibEvent'
 import { LIB_EVENT_NAMES } from '../../../../constants'
 import errors from '../../../../errors'
-// import { keplrChains } from '../../_BaseCosmosClass/signers/keplrChains'
+import { keplrChains } from '../../_BaseCosmosClass/signers/keplrChains'
 
 export async function setViewingKey(
   token,
@@ -15,42 +12,39 @@ export async function setViewingKey(
   { privateKey, derivationPath, viewingKey, fee = 0.003 } = {}
 ) {
   const networkClass = networkClasses.getNetworkClass(this.net)
+  let data
+  // separate flow for kepler
+  if (this.type === WALLET_TYPES.KEPLR) {
+    try {
+      const keplr = await this.getKeplr()
+      const chainId = keplrChains[this.net]
+      // set token to keplr
+      await keplr.suggestToken(chainId, networkClass.tokens[token].address)
+      // get token VK from keplr
+      const keplrViewingKey = await this.getViewingKeyByKeplr(token)
+      viewingKeyType = VIEWING_KEYS_TYPES.CUSTOM
+      data = { transactionHash: null, viewingKey: keplrViewingKey }
+    } catch (error) {
+      errors.throwError('KeplrError', { message: error.message })
+    }
+  } else {
+    if (this.balance.calculatedBalance < fee) {
+      errors.throwError('ViewingKeyError', { message: 'Insufficient funds' })
+    }
 
-  // // separate flow for kepler
-  // if (this.type === WALLET_TYPES.KEPLR) {
-  //   try {
-  //     const keplr = await this.getKeplr()
-  //     console.log('>>> keplr', keplr)
-  //     const chainId = keplrChains[this.net]
-  //     console.log('>>> chainId', chainId)
-  //     const data = await keplr.suggestToken(
-  //       chainId,
-  //       networkClass.tokens[token].address
-  //     )
-  //     console.log('>>> keplr data', data)
-  //     return
-  //   } catch (error) {
-  //     console.log('>>> error', error)
-  //     return
-  //   }
-  // }
-
-  if (this.balance.calculatedBalance < fee) {
-    errors.throwError('ViewingKeyError', { message: 'Insufficient funds' })
+    // set viewingKey
+    data = await snip20Manager.setViewingKey(viewingKeyType, {
+      address: this.address,
+      contractAddress: networkClass.tokens[token].address,
+      type: this.type,
+      publicKey: this.publicKey,
+      privateKey,
+      privateKeyHash: this.privateKeyHash,
+      derivationPath,
+      viewingKey,
+      fee,
+    })
   }
-
-  // set viewingKey
-  const data = await snip20Manager.setViewingKey(viewingKeyType, {
-    address: this.address,
-    contractAddress: networkClass.tokens[token].address,
-    type: this.type,
-    publicKey: this.publicKey,
-    privateKey,
-    privateKeyHash: this.privateKeyHash,
-    derivationPath,
-    viewingKey,
-    fee,
-  })
 
   // load balance
   await this.loadSnip20TokenBalance(

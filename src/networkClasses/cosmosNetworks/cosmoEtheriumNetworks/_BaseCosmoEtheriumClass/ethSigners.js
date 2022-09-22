bb6ca2fe43186db2c5caddce25cee763c65a5536
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2022-09-22 10:11:02 +0200

    fix: clean code

--- src/networkClasses/cosmosNetworks/cosmoEtheriumNetworks/_BaseCosmoEtheriumClass/ethSigners.js
+++ src/networkClasses/cosmosNetworks/cosmoEtheriumNetworks/_BaseCosmoEtheriumClass/ethSigners.js
@@ -22,11 +22,6 @@ const messageHash = (message) => {
 
 
 export const signTxByLedger = async (rawTransaction, derivationPath, publicKey) => {
-  console.log('test111',rawTransaction);
-  // const transaction = JSON.parse(rawTransaction)
-  // console.log('test111',transaction);
-  console.log(111,TypedDataUtils, SignTypedDataVersion);
-  // add global ledger app to avoid ledger reconnect error
   if (!global.ledger_eth && !global.ledger_bsc) {
     const transport = (await WebHidTransport.isSupported())
       ? await WebHidTransport.create(10000)
@@ -41,7 +36,6 @@ export const signTxByLedger = async (rawTransaction, derivationPath, publicKey) 
 
   const combined = `${r}${s}${v.toString(16)}`;
   const signature = combined.startsWith('0x') ? combined.slice(2) : combined;
-  // const signatureParsed = Buffer.from(signature).toString('hex');
 
   return {
     ...rawTransaction.message,
@@ -76,140 +70,3 @@ export const createMessageSignature = (message, privateKeyHex) => {
 
   return Buffer.from(signature).toString('hex')
 }
-
-// {
-//   "types": {
-//           "EIP712Domain": [
-//               {
-//                   "name": "name",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "version",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "chainId",
-//                   "type": "uint256"
-//               },
-//               {
-//                   "name": "verifyingContract",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "salt",
-//                   "type": "string"
-//               }
-//           ],
-//           "Tx": [
-//               {
-//                   "name": "account_number",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "chain_id",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "fee",
-//                   "type": "Fee"
-//               },
-//               {
-//                   "name": "memo",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "msgs",
-//                   "type": "Msg[]"
-//               },
-//               {
-//                   "name": "sequence",
-//                   "type": "string"
-//               }
-//           ],
-//           "Fee": [
-//               {
-//                   "name": "amount",
-//                   "type": "Coin[]"
-//               },
-//               {
-//                   "name": "gas",
-//                   "type": "string"
-//               }
-//           ],
-//           "Coin": [
-//               {
-//                   "name": "denom",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "amount",
-//                   "type": "string"
-//               }
-//           ],
-//           "Msg": [
-//               {
-//                   "name": "type",
-//                   "type": "string"
-//               },
-//               {
-//                   "name": "value",
-//                   "type": "MsgValue"
-//               }
-//           ]
-//           ,
-// "MsgValue": [
-//   {"name": "from_address", "type": "string"},
-//   {"name": "to_address", "type": "string"},
-//   {"name": "amount", "type": "TypeAmount[]"},
-// ],
-//       "TypeAmount": [
-//           {
-//               "name": "denom",
-//               "type": "string"
-//           },
-//           {
-//               "name": "amount",
-//               "type": "string"
-//           }
-//       ]
-//   },
-//   "primaryType": "Tx",
-//   "domain": {
-//       "name": "Injective Web3",
-//       "version": "1.0.0",
-//       "chainId": 17,
-//       "verifyingContract": "cosmos",
-//       "salt": "0"
-//   },
-//   "message": {
-//       "account_number": 8519,
-//       "chain_id": "injective-1",
-//       "fee": {
-//           "amount": [
-//               {
-//                   "amount": "187998000000000",
-//                   "denom": "inj"
-//               }
-//           ],
-//           "gas": "127998"
-//       },
-//       "memo": "transfer via citadel.one",
-//       "msgs": [
-//           {
-//               "type": "cosmos-sdk/MsgSend",
-//               "value": {
-//                   "amount": [
-//                       {
-//                           "amount": "1000000000000000",
-//                           "denom": "inj"
-//                       }
-//                   ],
-//                   "from_address": "inj18c5xvk63szp0tu79az7gad25rh5r3txkkgxf9g",
-//                   "to_address": "inj18c5xvk63szp0tu79az7gad25rh5r3txkkgxf9g"
-//               }
-//           }
-//       ],
-//       "sequence": 0
-//   }
-// }

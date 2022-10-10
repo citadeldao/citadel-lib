adf4a6862cf8d227a2ad170293dc77c6176f3320
Sergey Romanov <serega_rom@bk.ru>
2022-10-10 20:51:10 +0400

    fix: remove all consoles

--- src/networkClasses/cosmosNetworks/_BaseCosmosClass/signers/createMessageSignatureByPrivateKey.js
+++ src/networkClasses/cosmosNetworks/_BaseCosmosClass/signers/createMessageSignatureByPrivateKey.js
@@ -1,7 +1,6 @@
 const cosmos_lib = require('cosmos-lib')
 
 export const createMessageSignatureByPrivateKey = (data, privateKey) => {
-  console.log('>>> DATA', data)
   privateKey = privateKey.replace('0x', '')
   const signature = cosmos_lib.crypto.signJson(
     data,

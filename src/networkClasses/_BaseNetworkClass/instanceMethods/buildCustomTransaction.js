4a71f919e9b123f93df9d87476ed28ce4356e3b8
Sergey Romanov <serega_rom@bk.ru>
2022-10-25 01:03:31 +0400

    fix: buildCustomTransaction

--- src/networkClasses/_BaseNetworkClass/instanceMethods/buildCustomTransaction.js
+++ src/networkClasses/_BaseNetworkClass/instanceMethods/buildCustomTransaction.js
@@ -1,13 +1,13 @@
 import api from '../../../api'
 
 // by default - proxy request to backend
-export const buildCustomTransaction = async function (options) {
-  const { data } = await api.requests.buildCustomTransaction({
+export const buildCustomTransaction = async function (data) {
+  const { data: responseData } = await api.requests.buildCustomTransaction({
     net: this.net,
     address: this.address,
     publicKey: this.publicKey,
-    ...options,
+    data,
   })
 
-  return data
+  return responseData
 }

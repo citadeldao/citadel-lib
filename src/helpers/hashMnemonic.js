9e2070cc5596771744b31f7699e4717fbefc18c0
Sergey Romanov <serega_rom@bk.ru>
2022-10-25 18:31:38 +0400

    fix: hashMnemonic

--- src/helpers/hashMnemonic.js
+++ src/helpers/hashMnemonic.js
@@ -3,7 +3,7 @@ import CryptoJS from 'crypto-js'
 
 export const hashMnemonic = (mnemonic) => {
   if (mnemonic) {
-    return CryptoJS.SHA256(CryptoJS.lib.WordArray.create(mnemonic)).toString()
+    return CryptoJS.SHA256(mnemonic).toString()
   } else {
     return 'NO MNEMONIC'
   }

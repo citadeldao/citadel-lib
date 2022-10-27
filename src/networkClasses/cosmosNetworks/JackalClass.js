3b4a8cc7c328e774c56b65d4bfb0b64a1d80bfe4
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2022-10-27 18:53:07 +0200

    fix: fix jackal network

--- src/networkClasses/cosmosNetworks/JackalClass.js
+++ src/networkClasses/cosmosNetworks/JackalClass.js
@@ -4,4 +4,27 @@ export class JackalNetwork extends BaseCosmosNetwork {
   constructor(walletInfo) {
     super(walletInfo)
   }
+
+    // own explorer link
+    getScannerLinkById() {
+        return `https://explorer.nodestake.top/jackal/account/${this.address}`
+    }
+    
+    // own explorer link
+    getTransactionURLByHash(hash) {
+        return `https://explorer.nodestake.top/jackal/tx/${hash}`
+    }
+
+    static async createWalletByMnemonic(options) {
+        return super.createWalletByMnemonic(options, 'jkl')
+    }
+
+    static async createWalletByPrivateKey(options) {
+        return super.createWalletByPrivateKey(options, 'jkl')
+    }
+
+    static async createWalletByLedger(options) {
+        // alternative wallet factory
+        return super.createWalletByLedger(options, 'jkl')
+    }
 }

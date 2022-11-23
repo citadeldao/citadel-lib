0866f534f0b63f431735db0b679c5d629b3bc2c1
Sergey Romanov <serega_rom@bk.ru>
2022-11-23 15:43:16 +0400

    fix: tezos sodiumsumo bug

--- src/networkClasses/TezosClass/signers/functions.js
+++ src/networkClasses/TezosClass/signers/functions.js
@@ -1,6 +1,7 @@
 export const signMessageByEd25519 = async (msg, privateKey) => {
   // dynamic import of large module (for fast init)
   const { default: sodiumsumo } = await import('libsodium-wrappers-sumo')
+  await sodiumsumo.ready
   return Buffer.from(
     sodiumsumo.crypto_sign_detached(msg, Buffer.from(privateKey))
   ).toString('hex')

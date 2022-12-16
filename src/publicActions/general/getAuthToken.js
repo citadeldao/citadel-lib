2d6838a4caf29f4a4328acbadfcd870c72810dcc
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2022-12-16 09:47:18 +0100

    fix: fix getAuthToken, e-money to protobuff

--- src/publicActions/general/getAuthToken.js
+++ src/publicActions/general/getAuthToken.js
@@ -4,7 +4,7 @@ import api from '../../api'
 /**
  * Load exchange rates for all networks
  *
- * @returns Returns an object with network rates: { networkKey: ratesObj }
+ * @returns Returns and updates access token
  * When called outside, result wraps into an object of the form { result: 'success', data: returnedValue, error: null }
  * @example
  *

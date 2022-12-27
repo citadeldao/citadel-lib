e434effc99b2faf0969c79cb86bf9355db2068cd
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2022-12-27 08:39:21 +0100

    fix: add new time interval for coingeckoGetInfo

--- src/api/externalRequests/coingeckoGetTokensInfo.js
+++ src/api/externalRequests/coingeckoGetTokensInfo.js
@@ -2,7 +2,7 @@
 export const coingeckoGetTokensInfo = ({currency='usd', perPage=10, page=1}) => {
     return {
       // Full url for external request (for axios instance without BASE_URL)
-      url: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=1h,14h,30d`,
+      url: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=1h,24h,7d,30d`,
       method: 'get',
     }
   }

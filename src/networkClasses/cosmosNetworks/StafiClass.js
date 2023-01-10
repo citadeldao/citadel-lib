4cd443a355d14078db775f0639e22189ffd3a34c
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2023-01-10 18:07:35 +0100

    fix: add stafi explorer

--- src/networkClasses/cosmosNetworks/StafiClass.js
+++ src/networkClasses/cosmosNetworks/StafiClass.js
@@ -4,14 +4,4 @@ export class StafiNetwork extends BaseCosmosNetwork {
   constructor(walletInfo) {
     super(walletInfo)
   }
-
-  // own explorer link
-  getScannerLinkById() {
-    return `https://ping.pub/stafihub/account/${this.address}`
-  }
-
-  // own explorer link
-  getTransactionURLByHash(hash) {
-    return `https://ping.pub/stafihub/tx/${hash}`
-  }
 }

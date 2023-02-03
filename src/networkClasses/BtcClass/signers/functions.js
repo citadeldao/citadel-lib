1277d15de048bda030d652d7bbcc923757fae88e
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2023-02-03 22:21:22 +0100

    fix: add case for btc

--- src/networkClasses/BtcClass/signers/functions.js
+++ src/networkClasses/BtcClass/signers/functions.js
@@ -2,6 +2,12 @@ import { LEDGER_ERRORS, ERROR_CODES } from '../../../constants'
 import errors from '../../../errors'
 
 export function ledgerErrorHandler({ error, rightApp }) {
+    if(LEDGER_ERRORS.BTC.REJECT_ERROR_CODES.includes(+error.statusCode)){
+      errors.throwError('LedgerError', {
+        message: error.error_message,
+        code: ERROR_CODES.LEDGER.REJECT_CODE,
+      })
+    }
     if(LEDGER_ERRORS.BTC.WRONG_APP_CODES.includes(+error.statusCode)){
       errors.throwError('LedgerError', {
         message: error.error_message,

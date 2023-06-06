559b059dec29fec1e4df6059dad706f37f22eb42
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2023-06-06 13:27:59 +0200

    fix: add sui dao

--- src/networkClasses/SuiClass/index.js
+++ src/networkClasses/SuiClass/index.js
@@ -62,7 +62,21 @@ export class SuiNetwork extends BaseNetwork {
 
       return await signTxByLedger(transaction, derivationPath, this.publicKey, rightApp)
     }
-    return signTxByPrivateKey(transaction, privateKey)
+    return await signTxByPrivateKey(transaction, privateKey)
+  }
+
+  async createMessageSignature(rawTransaction, { privateKey, derivationPath }) {
+    const transaction = rawTransaction.transaction || rawTransaction
+    // ledger signer
+    if (this.type === WALLET_TYPES.LEDGER) {
+      //rigth app for ledger
+      const rightApp = storage.caches.getCache(CACHE_NAMES.NETWORKS_CONFIG)[this.net].ledger
+
+      return await signTxByLedger(transaction, derivationPath, this.publicKey, rightApp)
+    } 
+
+    // privateKey signer
+    return await signTxByPrivateKey(transaction, privateKey)
   }
 
   // async createMessageSignature(data, { privateKey, derivationPath }) {

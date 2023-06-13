f9f4948e5668eb9bfe4077a17f4f7cefec150e0c
Grigori Sargsyan <grigorisargsyan2016@gmail.com>
2023-06-13 22:31:03 +0200

    fix: add fiat arg to price history api

--- src/api/requests/network/getCurrencyHistoryByRange.js
+++ src/api/requests/network/getCurrencyHistoryByRange.js
@@ -1,14 +1,14 @@
 import state from '../../../state'
 
 // function returns request parameters for the axios instance.
-export const getCurrencyHistoryByRange = ({ net, dateFrom, dateTo }) => ({
+export const getCurrencyHistoryByRange = ({ net, dateFrom, dateTo, fiat = 'usd' }) => ({
   // backend domain is in the axios instance
   url: `currency/history`,
   method: 'get',
   data: {
     params: {
       net,
-      fiat: 'usd',
+      fiat,
       from_date: dateFrom,
       to_date: dateTo,
       version: state.getState('backendApiVersion'),
--- src/networkClasses/SuiClass/index.js
+++ src/networkClasses/SuiClass/index.js
@@ -66,20 +66,6 @@ export class SuiNetwork extends BaseNetwork {
   }
 
   async createMessageSignature(rawTransaction, { privateKey, derivationPath }) {
-    const transaction = rawTransaction.transaction || rawTransaction
-    // ledger signer
-    if (this.type === WALLET_TYPES.LEDGER) {
-      //rigth app for ledger
-      const rightApp = storage.caches.getCache(CACHE_NAMES.NETWORKS_CONFIG)[this.net].ledger
-
-      return await signTxByLedger(transaction, derivationPath, this.publicKey, rightApp)
-    } 
-
-    // privateKey signer
-    return await signTxByPrivateKey(transaction, privateKey)
-  }
-
-  async createMessageSignature(rawTransaction, { privateKey, derivationPath }) {
     let data
 
     // ledger signer

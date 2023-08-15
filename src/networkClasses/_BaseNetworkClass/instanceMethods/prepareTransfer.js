49b120af28dc9769dfed4315ee65c9b457f6e886
lapinp <nvzcsoft@yandex.ru>
2023-08-15 14:58:21 +0300

    fix: prepare transfer optional btcTypeAddress

--- src/networkClasses/_BaseNetworkClass/instanceMethods/prepareTransfer.js
+++ src/networkClasses/_BaseNetworkClass/instanceMethods/prepareTransfer.js
@@ -2,9 +2,16 @@ import api from '../../../api'
 
 // by default - proxy request to backend
 export const prepareTransfer = async function (options) {
+  let from = this.address;
+
+  if (options.btcAddressType) {
+    if (options.btcAddressType === 'segwit') from = this.segwitAddress;
+    if (options.btcAddressType === 'native') from = this.nativeAddress;
+  }
+
   const { data } = await api.requests.prepareTransfer({
     net: this.net,
-    from: this.address,
+    from,
     publicKey: this.publicKey,
     ...options,
   })

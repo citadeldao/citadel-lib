cf9a832829db78c808b97cefafeee712de2b64b5
lapinp <nvzcsoft@yandex.ru>
2023-10-25 16:55:33 +0300

    fix: fee key for generate route

--- src/api/requests/wallet/getCrossNetFees.js
+++ src/api/requests/wallet/getCrossNetFees.js
@@ -1,12 +1,20 @@
 import state from '../../../state'
 // function returns request parameters for the axios instance.
-export const getCrossNetFees = ({ netFrom, netTo }) => ({
+export const getCrossNetFees = ({ netFrom, netTo }) => {
+  let netKey = `${netFrom}_${netTo}`;
+  const toSplitNet = netTo.split('_');
+  
+  if (netFrom === toSplitNet[0]) {
+    netKey = netTo;
+  }
   // backend domain is in the axios instance
-  url: `/currency/${netFrom}_${netTo}/fees`,
-  method: 'get',
-  data: {
-    params: {
-      version: state.getState('backendApiVersion'),
+  return {
+    url: `/currency/${netKey}/fees`,
+    method: 'get',
+    data: {
+      params: {
+        version: state.getState('backendApiVersion'),
+      },
     },
-  },
-})
+  }
+}

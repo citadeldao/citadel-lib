f36eabaa1a308b6a3310f7866cddb9bb4d50cfc0
lapinp <nvzcsoft@yandex.ru>
2024-03-05 12:30:29 +0300

    fix: getTotalClaimedRewardsXCT add public action

--- src/publicActions/walletList/getTotalClaimedRewardsXCT.js
+++ src/publicActions/walletList/getTotalClaimedRewardsXCT.js
@@ -1,6 +1,6 @@
 import api from '../../api'
 
-export const getTotalClaimedRewardsXCT = async () => {
+export const getTotalClaimedRewardsXCT = async (address) => {
   const { data } = await api.requests.getTotalClaimedRewardsXct({
     address,
   })

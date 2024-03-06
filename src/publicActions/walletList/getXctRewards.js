aa0a5ae7d8f0acfa727e0b6e3d227e58f9c0ef8b
lapinp <nvzcsoft@yandex.ru>
2024-03-06 20:24:12 +0300

    fix: async

--- src/publicActions/walletList/getXctRewards.js
+++ src/publicActions/walletList/getXctRewards.js
@@ -1,6 +1,6 @@
 import api from '../../api'
 
-export const getXctRewards = (address) => {
+export const getXctRewards = async (address) => {
   // proxies a request to a backend
   const { data } = await api.requests.getXctRewards({
     address,

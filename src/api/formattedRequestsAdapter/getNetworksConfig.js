3025921544e54d75fd7a23601cc0ea60c4ddcf03
papabuylow <nvzcsoft@yandex.ru>
2024-03-14 12:51:47 +0300

    fix: return frontConfiguration from config network

--- src/api/formattedRequestsAdapter/getNetworksConfig.js
+++ src/api/formattedRequestsAdapter/getNetworksConfig.js
@@ -51,7 +51,7 @@ export const getNetworksConfig = async () => {
     delete formatedConfig[item.net].validatingRegExp
     delete formatedConfig[item.net].bridge
     // delete formatedConfig[item.net].connectorProps
-    delete formatedConfig[item.net].frontConfiguration
+    // delete formatedConfig[item.net].frontConfiguration
     delete formatedConfig[item.net].data
     // delete formatedConfig[item.net].primaryToken
     delete formatedConfig[item.net].unbondingPeriod

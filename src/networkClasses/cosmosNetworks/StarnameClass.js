f4ffe143ce6db1ec90864d9e2e461bda1df7c146
papabuylow <nvzcsoft@yandex.ru>
2024-04-09 10:44:13 +0300

    fix: edit explorer kujira, sifchain, starname

--- src/networkClasses/cosmosNetworks/StarnameClass.js
+++ src/networkClasses/cosmosNetworks/StarnameClass.js
@@ -3,4 +3,12 @@ export class StarnameNetwork extends BaseCosmosNetwork {
   constructor(walletInfo) {
     super(walletInfo)
   }
+
+  getScannerLinkById() {
+    return `https://atomscan.com/starname/accounts/${this.address}`
+  }
+
+  getTransactionURLByHash(hash) {
+    return `https://atomscan.com/starname/transactions/${hash}`
+  }
 }

6fc749fe80303c48492997253868f0fcc2626eb6
papabuylow <nvzcsoft@yandex.ru>
2024-11-07 21:48:49 +0300

    fix: fix sign by private key

--- src/networkClasses/SolanaClass/signers/signTxByPrivateKey.js
+++ src/networkClasses/SolanaClass/signers/signTxByPrivateKey.js
@@ -1,8 +1,11 @@
 const solanaWeb3 = require("@solana/web3.js");
 
 export const signTxByPrivateKey = async (rawTransaction, privateKey) => {
+  const buffPrv = Buffer.from(privateKey, 'hex');
+  const kPair = solanaWeb3.Keypair.fromSecretKey(Uint8Array.from(buffPrv));
+
   const restoredTx = solanaWeb3.VersionedTransaction.deserialize(rawTransaction.serialize());
-  restoredTx.sign([privateKey]);
+  restoredTx.sign([kPair]);
   const signedTx = Buffer.from(restoredTx.serialize()).toString('hex') ;
 
   return signedTx;

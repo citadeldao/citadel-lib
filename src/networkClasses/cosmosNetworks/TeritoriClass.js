642bfdc464f5cb609948aac0061e3f324cedf1ea
plapinp plapinp <plapinp17@gmail.com>
2025-02-07 09:30:41 +0300

    fix: update scanner juno, teritori

--- src/networkClasses/cosmosNetworks/TeritoriClass.js
+++ src/networkClasses/cosmosNetworks/TeritoriClass.js
@@ -5,4 +5,12 @@ export class TeritoriNetwork extends BaseCosmosNetwork {
   constructor(walletInfo) {
     super(walletInfo)
   }
+
+  getScannerLinkById() {
+    return `https://ping.pub/teritori/account/${this.address}`
+  }
+
+  getTransactionURLByHash(hash) {
+    return `https://ping.pub/teritori/tx/${hash}`
+  }
 }

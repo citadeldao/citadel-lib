da76c9590f7b13af03e0fb8456325b11ceeb6b7f
plapinp plapinp <plapinp17@gmail.com>
2025-04-08 10:27:37 +0300

    fix: add contractAddress to staking

--- src/api/publicRequests/wallet/buildLiquidStaking.js
+++ src/api/publicRequests/wallet/buildLiquidStaking.js
@@ -4,11 +4,10 @@ import qs from 'qs'
 // function returns request parameters for the axios instance.
 export const buildLiquidStaking = ({ address, publicKey, contractAddress, amount, action }) => {
   return {
-    url: `/blockchain/stacks/${address}/buildLiquidStacking?amount=${amount}&action=${action}`,
+    url: `/blockchain/stacks/${address}/buildLiquidStacking?amount=${amount}&action=${action}&contractAddress=${contractAddress}`,
     method: 'get',
     data: {
       params: {
-        contractAddress,
         publicKey,
         version: state.getState('backendApiVersion'),
       },
